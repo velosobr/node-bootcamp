7f872a505ff85b81b3a3123fc783380ab882e979
velosobr <linoc.veloso@gmail.com>
2019-12-20 10:03:44 -0300

    64 - parsing incoming requests

--- 04 - express/app.js
+++ 04 - express/app.js
@@ -2,14 +2,16 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log("In a middleware");
-  next() // this alows the request to continue to the next middleware
-})
-
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
   console.log("In another middleware");
 
+  res.send(
+    '<form><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  )
+})
+
+app.use('/', (req, res, next) => {
+  console.log("In another middleware");
   res.send('<h1>Hello from Express</h1>')
 
 })
