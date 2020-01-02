f5957d3689ce1d8aefb66d3b43e0be57216b7d2c
velosobr <linoc.veloso@gmail.com>
2020-01-01 23:50:11 -0300

    72 - Using a helper function for navigation

--- 04 - express/app.js
+++ 04 - express/app.js
@@ -7,14 +7,14 @@ const app = express()
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
-
+app.use((req, res, nex) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3333)
 
--- 04 - express/routes/shop.js
+++ 04 - express/routes/shop.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const path = require('path')
 
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.send("<h1>Hello from the other side!</h1>")
+  res.sendFile(
+    path.join(__dirname, '../', 'views', 'shop.html')
+  )
 })
 
 module.exports = router;
