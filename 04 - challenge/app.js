2a08647e88a891ddba053daa95a575cbe9464583
velosobr <linoc.veloso@gmail.com>
2020-01-05 00:17:13 -0300

    more about the starting process

---
+++ 04 - challenge/app.js
@@ -0,0 +1,14 @@
+/**
+ * 1 - Create a NPM project and install Express.js (Nodemon if you want)
+ * 2 - Create an Express.js app which serves two HTML files (of your choice/with
+ *  your content) for / and /users.
+ * 3 - Add some static (.js or .css) files to your project that should be 
+ * required by at least one of your HTML files.
+ *
+ */
+
+const express = require('express')
+
+const app = express()
+
+app.listen(4444)
