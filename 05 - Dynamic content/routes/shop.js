38ea92a78d361cd7301f7f647cae7e42b1c8915b
velosobr <linoc.veloso@gmail.com>
2020-01-05 21:38:10 -0300

    78 - sharing data across requests & users

--- 05 - Dynamic content/routes/shop.js
+++ 05 - Dynamic content/routes/shop.js
@@ -6,7 +6,11 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const adminData = require('./admin')
+
 router.get('/', (req, res, next) => {
+  console.log('shop.js: ', adminData.products);
+
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
